fce2be7c57e738f8a3adb23a3e5321fda66a0480
Irfan Hodzic <irfan.hodzic@viesure.io>
2021-11-02 15:33:29 +0100

    First commit

---
+++ src/main/java/irfan/springexample/spring5webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package irfan.springexample.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
