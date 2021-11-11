b9dc70e4ab99a681417908372c568e03db9d4efa
Irfan Hodzic <irfan.hodzic@viesure.io>
2021-11-11 16:11:08 +0100

    Added controller for author, thymeleaf template

--- src/main/java/irfan/springexample/spring5webapp/controller/BookController.java
+++ src/main/java/irfan/springexample/spring5webapp/controller/BookController.java
@@ -1,2 +1,22 @@
-package irfan.springexample.spring5webapp.controller;public class BookController {
+package irfan.springexample.spring5webapp.controller;
+
+import irfan.springexample.spring5webapp.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
 }
